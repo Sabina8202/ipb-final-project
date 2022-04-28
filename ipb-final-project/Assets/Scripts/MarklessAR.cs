3a99c2d787b3d864b75b887126e4d03a5fd26f3e
Nitu Giri <66070427+NituGiri@users.noreply.github.com>
2022-04-28 03:56:03 +0100

    MaklessAR
    did few tests not finished yet though

---
+++ ipb-final-project/Assets/Scripts/MarklessAR.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarklessAR : MonoBehaviour
+{
+  //Gyro
+  private Gyroscope gyro;
+  private GameObject cameraContainer;
+  private Quaternion rotation;
+
+    //Camera
+    private WebCamTexture cam;
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private bool arReady = false;
+
+    private void Start(){
+        //Check if we support both services
+        //Gyroscope
+        if (!SystemInfo.supportsGyroscope){
+            Debug.Log("No Gyroscope Support");
+            return;
+        }
+        //Back Camera
+        for (int i = 0; i < WebCamTexture.devices.Length; i++){
+            if (!WebCamTexture.devices[i].isFrontFacing){
+                cam = new WebCamTexture(WebCamTexture.devices[i].name, Screen.width, Screen.height);
+                break;
+
+      }
+     }
+
+        //Check if we have a back camera
+        if(cam == null){
+            Debug.Log("No  Back Camera Found");
+            return;
+        }
+        //Both services are supported ,let's enable them
+        cameraContainer= new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyro = Input.gyro;
+        gyro.enabled = true;
+
+        cam.Play();
+        background.texture = cam;
+
+        arReady = true;
+    }
+
+    private void Update(){
+        if (!arReady){
+            //Update the camera
+            float ratio = (float)cam.width / (float)cam.height;
+            fit.aspectRatio = ratio;
+
+            float scaleY = cam.videoVerticallyMirrored ? -1.0f : 1.0f;
+            background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+            int orient = -cam.videoRotationAngle;
+            background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+
+
+            //Update Gyro
+            transform.localRotation = gyro.attitude * rotation;
+        }
+            
+    }
+}
